8c0a20587233ac53ae60d34f0ae6761c2df4f3fc
Sidharth B. <sidharthtechworld2k@gmail.com>
2026-06-17 07:03:32 +0530

    Created rough signup and login page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/signup',(req,res)=>{
+  res.render('signup_page')
+})
+router.get('/login',(req,res)=>{
+  res.render('login_page')
+})
 module.exports = router;
