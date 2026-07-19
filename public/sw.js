a7791a8773aeb81c0b0ee890c2a734ed7b5d7452
Sidharth B <sidharthtechworld2k@gmail.com>
2026-07-19 15:54:43 +0530

    Update service worker for improved caching and modify Google site verification meta tag

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,26 @@
+const CACHE_NAME = "doubthub-v1";
+
 self.addEventListener("install", (event) => {
-  console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/manifest.json",
+        "/stylesheets/style.css"
+      ]);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker Activated");
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
