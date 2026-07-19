ee3d4a1f54378ea9706e292d13ed15ec939c6d1a
Sidharth B <sidharthtechworld2k@gmail.com>
2026-07-19 15:34:23 +0530

    Add service worker registration and implementation for improved PWA support

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker Installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker Activated");
+});
